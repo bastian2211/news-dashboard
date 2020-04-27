a738876d452d686b7acedd61add5342deb88a777
bastian2211 <Bastian2211@web.de>
2020-04-27 11:13:22 +0200

    Add Postgres configuration

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,24 @@
-require('dotenv').config();
-module.exports = {
-    client: 'mysql',
-    connection: {
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE
+if (process.env.DATABASE_URL) {
+  module.exports = {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 1,
+      max: 100,
     },
-    pool: { 
-        min: 1,
-        max: 100
-    }
-};
+  };
+} else {
+  require("dotenv").config();
+  module.exports = {
+    client: "mysql",
+    connection: {
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+    },
+    pool: {
+      min: 1,
+      max: 100,
+    },
+  };
+}
